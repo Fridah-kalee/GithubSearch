af0a22ba08a84069366aea8adbc4bcfc0e411a61
Fridah-kalee <fridahkalee@gmail.com>
2022-04-17 10:12:08 +0300

    Added github token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  githubApiToken: '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'
 };
 
 /*
